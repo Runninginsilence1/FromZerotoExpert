104103274e142171939ba268d1d500f442f99487
Runninginsilence <runninginsilence0@gmail.com>
2022-09-16 21:58:14 +0800

    一个可以部署的Web服务。 应该符合第二个打卡的要求了...

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Complete a JavaWeb Project. 帅地从零到一个完整的JavaWeb项目。
 # 已完成部分
 1. GitHub以及Git集成已完成，所有的push现在都在 dev 分支。
 1. 因为JavaWeb是很久以前看的，现在回顾了一下 Servlet的内容，使用IDEA 的JavaEE模板构建了一个Web项目（里面甚至还有一个jsp文件）。因为零基础，所以暂时写了一个自定义的Servlet，可以访问。
+
+# 实现
+## 创建一个Web服务
+参考的是B站播放量高的黑马的一个视频。
+自己下载 Tomcat，与 IDEA集成，然后创建一个 包含 Maven和Web规范的JavaEE项目。
+自己在src里面找到默认的模板，仿照写一个功能是 输出指定内容的Servlet即可。
+
+打卡的要求是 要求打开 
+> 创建一个 web 服务，在浏览器输入http://localhost/FromZerotoExpert这个地址后，出现一个 html 页面，页面展示 嗨，欢迎您来到 from zero to expert.。
+
+Servlet的的部分没什么问题，但是设置 端口为80和 配置资源路径是靠自己摸索出来的。
+端口的部分我知道改，但是嫌麻烦和排查原因困难所以使用的默认端口，默认URL的话是改的 Tomcat的配置选项：
+Edit Configuration -> Deployment -> Application Context 设置为 **一个斜杠**。代表默认的**项目启动路径就是根路径**。
--- src/main/java/com/example/FromZerotoExpert/FirstServlet.java
+++ src/main/java/com/example/FromZerotoExpert/FirstServlet.java
@@ -8,9 +8,12 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "FromZerotoExpert", value = "/FromZerotoExpert")
 public class FirstServlet extends HttpServlet {
+//    private String message = "";
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 写一个数据：你学会Servlet了吗？
+        String message = "嗨，欢迎您来到 from zero to expert.";
+        // 我反正理解成 Golang的 handler机制了，我觉得是一个东西，可能其他语言也是这么做的
         System.out.println("doGet被执行");
         // 回复一个正常的响应
         // 响应行
@@ -19,9 +22,10 @@ public class FirstServlet extends HttpServlet {
         // 这个应该是封装的那个方法
         response.setContentType("text/html;charset=utf-8");
 //        response.setHeader("Content-Type", "text/html");
-        PrintWriter writer = response.getWriter();
-        response.setCharacterEncoding("utf-8");
-        writer.write("<h1>我不想乱码！</h1>");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
 
 //        这个流不需要关闭，由 tomcat 进行管理
     }
